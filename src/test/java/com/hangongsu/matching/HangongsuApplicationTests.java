09252429b644a2fe51923048cfeca40765b6b940
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-08-16 11:11:46 +0900

    :tada: 개인 프로젝트 시작

---
+++ src/test/java/com/hangongsu/matching/HangongsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangongsu.matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangongsuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
