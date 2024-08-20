29b656f8c9c3f5ed276fb4a87ff3603fb0c48476
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-08-20 21:17:40 +0900

    :truck: Application.java API 모듈 이동

--- hangongsu-api/src/main/java/com/hangongsu/api/HangongsuApplication.java
+++ hangongsu-api/src/main/java/com/hangongsu/api/HangongsuApplication.java
@@ -1,11 +1,13 @@
 package com.hangongsu.api;
 
-import com.hangongsu.support.common.config.EnablePropertiesConfig;
+import com.hangongsu.core.common.ServiceConfig;
+import com.hangongsu.infrastructure.common.InfraStructureConfig;
+import com.hangongsu.support.common.config.SupportConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(EnablePropertiesConfig.class)
+@Import({SupportConfig.class, ServiceConfig.class, InfraStructureConfig.class})
 @SpringBootApplication
 public class HangongsuApplication {
 
