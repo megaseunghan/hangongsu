1bc748c21d3ed69ef292e96812c2b24c087c32e5
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-08-20 20:52:54 +0900

    :sparkles: 소셜 로그인 요청 API AuthController 추가

--- hangongsu-api/src/main/java/com/hangongsu/api/controller/auth/OAuthController.java
+++ hangongsu-api/src/main/java/com/hangongsu/api/controller/auth/OAuthController.java
@@ -1,12 +1,15 @@
 package com.hangongsu.api.controller.auth;
 
-import com.hangongsu.api.provider.auth.OAuthRedirectUriProvider;
-import com.hangongsu.core.application.auth.OAuthLoginService;
+import com.hangongsu.api.provider.auth.OAuthRedirectUriProviderComposite;
 import com.hangongsu.core.domain.user.vo.Platform;
+import com.hangongsu.core.dto.auth.response.LoginResponse;
+import com.hangongsu.core.port.auth.in.OAuthLoginService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.net.URI;
 
 @RestController
@@ -14,18 +17,21 @@ import java.net.URI;
 @RequestMapping("/api/v1/oauth")
 public class OAuthController {
 
-    private final OAuthRedirectUriProvider redirectUriProvider;
+    private final OAuthRedirectUriProviderComposite redirectUriProvider;
     private final OAuthLoginService oAuthLoginService;
 
     @GetMapping("/{platform}/login")
-    public ResponseEntity<Void> redirectLoginPage(@PathVariable String platform) {
+    public ResponseEntity<Void> redirectLoginPage(@PathVariable String platform, HttpServletResponse response) throws IOException, IllegalArgumentException {
         URI redirectUri = redirectUriProvider.generateRedirectUri(Platform.from(platform));
+        response.sendRedirect(redirectUri.toString());
+
         return ResponseEntity.created(redirectUri).build();
     }
 
     @GetMapping("/{platform}/callback")
-    public OAuthLoginResponse callback(@RequestParam(name = "code") String authorizationCode, @PathVariable String platform) {
-        return oAuthLoginService.login(authorizationCode, platform);
+    public ResponseEntity<LoginResponse> callback(@RequestParam(name = "code") String authorizationCode, @PathVariable String platform) {
+        LoginResponse response = oAuthLoginService.login(authorizationCode, Platform.from(platform));
+        return ResponseEntity.ok(response);
     }
 }
 
