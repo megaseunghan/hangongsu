fe3045f16465971546137087f42196c47b4dcf88
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-08-22 17:09:05 +0900

    :recycle: @SpringBootApplication @Import 어노테이션 ModuleImportConfig 클래스파일로 분리
    - :truck: ServiceConfig -> CoreConfig 이름 변경

--- hangongsu-core/src/main/java/com/hangongsu/core/common/CoreConfig.java
+++ hangongsu-core/src/main/java/com/hangongsu/core/common/CoreConfig.java
@@ -3,5 +3,5 @@ package com.hangongsu.core.common;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com.hangongsu.core", lazyInit = true)
-public class ServiceConfig {
+public class CoreConfig {
 }
