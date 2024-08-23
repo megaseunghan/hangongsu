12f8a26d8eac5cb5ad4bc87e1041ce0ab407a47f
megaseunghan <84169773+megaseunghan@users.noreply.github.com>
2024-08-23 15:15:56 +0900

    :recycle: UserServiceImpl 비즈니스 로직 분리
    - UserService -> UserLoginService, UserSignupService 분리

--- hangongsu-core/src/main/java/com/hangongsu/core/application/auth/OAuthLoginServiceImpl.java
+++ hangongsu-core/src/main/java/com/hangongsu/core/application/auth/OAuthLoginServiceImpl.java
@@ -7,7 +7,7 @@ import com.hangongsu.core.dto.auth.response.TokenResponse;
 import com.hangongsu.core.dto.auth.response.UserInfoResponse;
 import com.hangongsu.core.port.auth.in.OAuthLoginService;
 import com.hangongsu.core.port.auth.out.TokenClient;
-import com.hangongsu.core.port.user.in.UserService;
+import com.hangongsu.core.port.user.in.UserLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 public class OAuthLoginServiceImpl implements OAuthLoginService {
 
     private final OAuthClientComposite oAuthClient;
-    private final UserService userService;
+    private final UserLoginService userLoginService;
     private final TokenClient tokenClient;
 
     @Override
@@ -24,7 +24,7 @@ public class OAuthLoginServiceImpl implements OAuthLoginService {
         String accessToken = oAuthClient.getAccessToken(authorizationCode, platform);
         UserInfoResponse userInfo = oAuthClient.retrieveUserInfo(accessToken, platform);
         TokenResponse token = tokenClient.createAccessToken(userInfo);
-        User user = userService.registerIfAbsent(userInfo);
+        User user = userLoginService.login(userInfo);
 
         return LoginResponse.from(user.getUserId(), token);
     }
--- hangongsu-core/src/main/java/com/hangongsu/core/port/user/in/UserLoginService.java
+++ hangongsu-core/src/main/java/com/hangongsu/core/port/user/in/UserLoginService.java
@@ -3,6 +3,6 @@ package com.hangongsu.core.port.user.in;
 import com.hangongsu.core.domain.user.entity.User;
 import com.hangongsu.core.dto.auth.response.UserInfoResponse;
 
-public interface UserService {
-    User registerIfAbsent(UserInfoResponse userInfoResponse);
+public interface UserLoginService {
+    User login(UserInfoResponse userInfoResponse);
 }
